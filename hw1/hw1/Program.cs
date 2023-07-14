072da199b405b4ba46cee5e2b4bf83c45b6c9622
shangfengh <3495281661@qq.com>
2023-07-14 18:05:04 +0800

    build: :tada: assign the homework

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -1,92 +1,90 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace Homework1
+namespace Homework
 {
     public class Program
     {
-        public static void Main(string[] args)  // 该程序用于Debug
+        public static void Main(string[] args) 
         {
-            IProgress progress = new Progress();
-            for (int i = 1; i <= 20; i++)
-            {
-                try
-                {
-                    progress.Start(i*((i%2==0)?1:-1));
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                if (i % 3 != 0)
-                {
-                    progress.Add(i-1);
-                }
-                else progress.Sub(i-1);
+            ILongProgressByTime a = new LongProgressByTime();
+            new Thread
+                (
+                    () =>
+                    {
+                        Console.WriteLine("A Start: "+(a.Start(2000)).ToString());
+                        Thread.Sleep(1000);
+                        Console.WriteLine("A TrySet0: "+(a.TrySet0()).ToString());
+                        Thread.Sleep(500);
+                        Console.WriteLine("A Start: "+(a.Start(1000)).ToString() +" Now: "+Environment.TickCount64);
+                        Thread.Sleep(500);
+                        Console.WriteLine("A Progress: "+(a.GetProgress()).ToString() +" Now: "+Environment.TickCount64);
+                        Thread.Sleep(1003);
+                        Console.WriteLine("A TrySet0: "+(a.TrySet0()).ToString());
+                    }
+                ).Start();
 
-                if (i == 6 || i == 7)
-                {
-                    progress.Double();
-                }
-                Console.WriteLine(progress.GetProgress().ToString());
-            }
+            new Thread
+                (
+                    () =>
+                    {
+                        Console.WriteLine("B Start: "+(a.Start(2000)).ToString());
+                        Thread.Sleep(1500);
+                        Console.WriteLine("B Start: " +(a.Start(1000)).ToString() + " Now: " + Environment.TickCount64);
+                        Thread.Sleep(500);
+                        Console.WriteLine("B Progress: " +(a.GetProgress()).ToString() + " Now: " + Environment.TickCount64);
+                    }
+                ).Start();
         }
     }
 
-    public interface IProgress
+    public interface ILongProgressByTime
     {
-        public int Num { get; } // Progress的序号，表明是第几个实例化的Progress
-        public int RequiredProgress { get; } // Progress加载完成所需进度
-        public int FinishedProgress { get; } // FinishedProgress指其中已完成的进度, FinishedProgress应当在[0,RequiredProgress]中
+        /// <summary>
+        /// 尝试加载下一次进度条，needTime指再次加载进度条所需时间，单位毫秒
+        /// 如果之前进度条已经终止，则将进度开始下一次加载，返回true
+        /// 如果之前进度条尚未终止，返回false
+        /// </summary>
+        public bool Start(long needTime);
 
         /// <summary>
-        /// 尝试加载下一次进度条，requiredProgress指再次加载进度条所需进度
-        /// 如果之前进度条已经加载完成，则将进度清零开始下一次加载，返回true，但如果requiredProgress<0，应当报错
-        /// 如果之前进度条尚未加载完成，返回false
+        /// 使未完成的进度条终止清零，返回值代表是否成功终止
         /// </summary>
-        public bool Start(int requiredProgress); 
-        
-        public void Add(int addProgress); //增加addProgress的进度
-        public void Sub(int subProgress); //减少subProgress的进度
-        public void Double(); //进度翻倍
+        public bool TrySet0();
 
         /// <summary>
-        ///  FinishedProgress指其中已完成的进度，RequiredProgress指当前Progress完成所需进度
+        /// 使进度条强制终止清零
         /// </summary>
-        public (int FinishedProgress, int RequiredProgress) GetProgress();
+        public void Set0();
+
+        /// <summary>
+        ///     ElapsedTime指其中已过去的时间，NeedTime指当前Progress完成所需时间，单位毫秒
+        /// </summary>
+        public (long ElapsedTime, long NeedTime) GetProgress();
     }
 
-    public class Progress : IProgress
+    public class LongProgressByTime: ILongProgressByTime
     {
-        // 一个进度条
-        // 只允许修改Progress类中的代码
-        // 要求实现IProgress中的要求
+        // 根据时间推算Start后完成多少进度的进度条（long）。
+        // 只允许Start时修改needTime（确保较大）；
+        // 支持TrySet0使未完成的进度条终止清零；
+
+        // 只允许修改LongProgressByTime类中的代码
+        // 要求实现ILongProgressByTime中的要求
+        // 可利用Environment.TickCount64获取当前时间（单位ms）
+
+        //挑战：利用原子操作
+        //long.MaxValue非常久
     }
 
-/*
- * 输出示例：
-RequiredProgress must be positive. (Parameter 'Homework1.Progress')
-(0, 0)
-(1, 2)
-(0, 2)
-(2, 2)
-RequiredProgress must be positive. (Parameter 'Homework1.Progress')
-(2, 2)
-(0, 6)
-(6, 6)
-(7, 8)
-(0, 8)
-(8, 8)
-RequiredProgress must be positive. (Parameter 'Homework1.Progress')
-(8, 8)
-(0, 12)
-(12, 12)
-(13, 14)
-(0, 14)
-(14, 14)
-RequiredProgress must be positive. (Parameter 'Homework1.Progress')
-(14, 14)
-(0, 18)
-(18, 18)
-(19, 20)
- */
+/*输出示例：
+ * A Start: False
+B Start: True
+A TrySet0: True
+B Start: True Now: 14536562
+A Start: False Now: 14536578
+B Progress: (516, 1000) Now: 14537078
+A Progress: (516, 1000) Now: 14537078
+A TrySet0: False
+*/
 }
