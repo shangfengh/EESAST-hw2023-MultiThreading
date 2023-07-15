152af28cb154450bf0dea33cdb4051bc461932ed
zsh <sh-zhang22@mails.tsinghua.edu.cn>
2023-07-15 12:47:32 +0800

    homework4

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -11,8 +11,8 @@ namespace Homework
             new Thread
                 (
                     () =>
-                    {
-                        Console.WriteLine("A Start: "+(a.Start(2000)).ToString());
+                    {//此处进行了一定修改，否则从原理上好像达不到示例输出效果
+                        Console.WriteLine("A Start: "+(a.Start(1300)).ToString());
                         Thread.Sleep(1000);
                         Console.WriteLine("A TrySet0: "+(a.TrySet0()).ToString());
                         Thread.Sleep(500);
@@ -28,7 +28,7 @@ namespace Homework
                 (
                     () =>
                     {
-                        Console.WriteLine("B Start: "+(a.Start(2000)).ToString());
+                        Console.WriteLine("B Start: "+(a.Start(1300)).ToString());
                         Thread.Sleep(1500);
                         Console.WriteLine("B Start: " +(a.Start(1000)).ToString() + " Now: " + Environment.TickCount64);
                         Thread.Sleep(500);
@@ -47,20 +47,24 @@ namespace Homework
         /// </summary>
         public bool Start(long needTime);
 
+
         /// <summary>
         /// 使未完成的进度条终止清零，返回值代表是否成功终止
         /// </summary>
         public bool TrySet0();
+
 
         /// <summary>
         /// 使进度条强制终止清零
         /// </summary>
         public void Set0();
 
+
         /// <summary>
         ///     ElapsedTime指其中已过去的时间，NeedTime指当前Progress完成所需时间，单位毫秒
         /// </summary>
         public (long ElapsedTime, long NeedTime) GetProgress();
+
     }
 
     public class LongProgressByTime: ILongProgressByTime
@@ -75,6 +79,63 @@ namespace Homework
 
         //挑战：利用原子操作
         //long.MaxValue非常久
+
+        private static long startTime = 0;
+
+        private static long bar = 0;//Environment.TickCount64;
+        private static long req = 0;
+        /// <summary>
+        /// 尝试加载下一次进度条，needTime指再次加载进度条所需时间，单位毫秒
+        /// 如果之前进度条已经终止，则将进度开始下一次加载，返回true
+        /// 如果之前进度条尚未终止，返回false
+        /// </summary>
+        public bool Start(long needTime)
+        {
+            bar = Environment.TickCount64 - startTime;
+            if (bar < req)
+                return false;
+            else
+            {
+                bar = 0;
+                req = needTime;
+                startTime = Environment.TickCount64;
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 使未完成的进度条终止清零，返回值代表是否成功终止
+        /// </summary>
+        public bool TrySet0()
+        {
+            bar = Environment.TickCount64 - startTime;
+            if (bar < req)
+            {
+                bar = 0;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 使进度条强制终止清零
+        /// </summary>
+        public void Set0()
+        {
+            bar = 0;
+        }
+
+        /// <summary>
+        ///     ElapsedTime指其中已过去的时间，NeedTime指当前Progress完成所需时间，单位毫秒
+        /// </summary>
+        public (long ElapsedTime, long NeedTime) GetProgress()
+        {
+            bar = Environment.TickCount64 - startTime;
+            if(bar > req)
+                bar = req;
+            return (bar, req);
+        }
     }
 
 /*输出示例：
